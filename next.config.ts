d4df7e3ada2686ef4c777973ab6961614c2cfdb2
Ketikweb <ketikwebcorp@gmail.com>
2025-01-01 00:45:20 +0700

    Set assetPrefix for production environment in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: isProd ? '/ketikweb' : undefined,
 };
 
 export default nextConfig;
